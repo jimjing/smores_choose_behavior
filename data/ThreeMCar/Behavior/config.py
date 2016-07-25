9269c1aab932523a4c715c54b99866d3bc823861
Tarik Tosun <ttosun89@gmail.com>
2016-07-25 19:19:10 -0400

    [CORE] Added config.py.

---
+++ data/ThreeMCar/Behavior/config.py
@@ -0,0 +1,24 @@
+# config.py 
+
+"""
+Simulator names:
+
+0-1-2
+
+"""
+
+from numpy import pi
+
+angle = 90 
+ModuleMap = {'SMORES_0':, 'SMORES_1':12, 'SMORES_2':11 } 
+                        }
+NeutralPositions = {} 
+
+disabledDof=[]
+speed_scale=0.7
+behaviorFiles = {
+   'forward':'ThreeMCarF.xml',
+   'right':  'ThreeMCarR.xml',
+   'left':  'ThreeMCarL.xml',
+   }
+
