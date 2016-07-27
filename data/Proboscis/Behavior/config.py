e557f339a9c5ea5f44a59731286aa42f6d20feb6
Tarik Tosun <ttosun89@gmail.com>
2016-07-27 17:16:55 -0400

    [DATA] Edited config.py for Proboscis

--- data/Proboscis/Behavior/config.py
+++ data/Proboscis/Behavior/config.py
@@ -11,15 +11,16 @@ Simulator names:
 from numpy import pi
 
 angle = 90
-ModuleMap = {'SMORES_0':14, 'SMORES_1':4, 'SMORES_2':12, 'SMORES_3':12,
-             'SMORES_4':12, 'SMORES_5':12,'SMORES_6':12,}
+ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':14, 'SMORES_3':3,
+             'SMORES_4':16, 'SMORES_5':9,'SMORES_6':8,}
 NeutralPositions = {}
 
-disabledDof=[]
+disabledDof=['pan']
 speed_scale=0.7
 behaviorFiles = {
    'ProboscisOverBar.xml':'ProboscisOverBar.xml',
    'ProboscisPickup.xml':'ProboscisPickup.xml',
    'ProboscisBackOverBar.xml':'ProboscisBackOverBar.xml',
+   'init':'ProboscisPostReconf.xml',
    }
 
