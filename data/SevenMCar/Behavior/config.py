51b9161c5debdc776576e7383dc58aa909e99e40
Jim Jing <{u'verified': True, u'email': u'jimjing131313@gmail.com', u'primary': True}>
2016-07-27 17:32:40 -0400

    [DATA] Added SevenMCar example.

---
+++ data/SevenMCar/Behavior/config.py
@@ -0,0 +1,24 @@
+# config.py
+
+"""
+Simulator names:
+
+4   6
+1-0-2
+3   5
+"""
+
+from numpy import pi
+
+angle = 90
+ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':14, 'SMORES_3':3,
+             'SMORES_4':16, 'SMORES_5':9,'SMORES_6':8,}
+NeutralPositions = {}
+
+disabledDof=['pan']
+speed_scale=0.7
+behaviorFiles = {
+   'SevenMCarDiff.xml':'SevenMCarDiff.xml',
+   'SevenMCarPreReconf.xml':'SevenMCarPreReconf.xml',
+   }
+
