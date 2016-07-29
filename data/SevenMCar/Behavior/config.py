c767d7e92ebb08dcf8027e229471358a46aecd06
Tarik Tosun <tarikt@seas.upenn.edu>
2016-07-29 00:19:33 -0400

    [DATA] >odified config.py for SevenMCar.

--- data/SevenMCar/Behavior/config.py
+++ data/SevenMCar/Behavior/config.py
@@ -11,8 +11,8 @@ Simulator names:
 from numpy import pi
 
 angle = 90
-ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':14, 'SMORES_3':3,
-             'SMORES_4':16, 'SMORES_5':9,'SMORES_6':2,}
+ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':2, 'SMORES_3':3,
+             'SMORES_4':16, 'SMORES_5':3,'SMORES_6':12,}
 NeutralPositions = {}
 
 disabledDof=['tilt']
