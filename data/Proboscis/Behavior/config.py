b70cd6ad25a57200858c380a3e3dbf302989ef52
Tarik Tosun <ttosun89@gmail.com>
2016-07-29 13:50:16 -0400

    added common name_map for proboscis and sevenmodcar

--- data/Proboscis/Behavior/config.py
+++ data/Proboscis/Behavior/config.py
@@ -1,4 +1,7 @@
 # config.py
+import sys
+sys.path.append('../..')
+import name_map
 
 """
 Simulator names:
@@ -11,8 +14,10 @@ Simulator names:
 from numpy import pi
 
 angle = 90
-ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':14, 'SMORES_3':3,
-             'SMORES_4':16, 'SMORES_5':9,'SMORES_6':8,}
+#ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':14, 'SMORES_3':3,
+#             'SMORES_4':16, 'SMORES_5':9,'SMORES_6':8,}
+ModuleMap = {'SMORES_0':middle, 'SMORES_1':back_middle, 'SMORES_2':front_middle, 'SMORES_3':back_right,
+             'SMORES_4':back_left, 'SMORES_5':front_right,'SMORES_6':front_left,}
 NeutralPositions = {}
 
 disabledDof=['pan']
--- data/SevenMCar/Behavior/config.py
+++ data/SevenMCar/Behavior/config.py
@@ -1,4 +1,7 @@
 # config.py
+import sys
+sys.path.append('../../')
+import name_map
 
 """
 Simulator names:
@@ -11,11 +14,14 @@ Simulator names:
 from numpy import pi
 
 angle = 90
-ModuleMap = {'SMORES_0':16, 'SMORES_1':18, 'SMORES_2':2, 'SMORES_3':14,
-             'SMORES_4':13, 'SMORES_5':12,'SMORES_6':9,}
+#ModuleMap = {'SMORES_0':16, 'SMORES_1':18, 'SMORES_2':2, 'SMORES_3':14,
+#             'SMORES_4':13, 'SMORES_5':12,'SMORES_6':9,}
+ModuleMap = {'SMORES_0':middle, 'SMORES_1':back_middle, 'SMORES_2':front_middle, 'SMORES_3':back_right,
+             'SMORES_4':back_left, 'SMORES_5':front_right,'SMORES_6':front_left,}
+
 NeutralPositions = {}
 
-disabledDof=['tilt']
+disabledDof=[]
 speed_scale=0.7
 behaviorFiles = {
    'SevenMCarDiff.xml':'SevenMCarDiff.xml',
