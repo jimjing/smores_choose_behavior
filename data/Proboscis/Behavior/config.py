ab31083ef8c4f73f9586b54edb7f57335fc1cebc
Tarik Tosun <ttosun89@gmail.com>
2016-07-29 22:14:23 -0400

    [DATA] Capitalization and periods.  And config.py.

--- data/Proboscis/Behavior/config.py
+++ data/Proboscis/Behavior/config.py
@@ -17,15 +17,18 @@ angle = 90
 #ModuleMap = {'SMORES_0':13, 'SMORES_1':12, 'SMORES_2':14, 'SMORES_3':3,
 #             'SMORES_4':16, 'SMORES_5':9,'SMORES_6':8,}
 ModuleMap = {'SMORES_0':middle, 'SMORES_1':back_middle, 'SMORES_2':front_middle, 'SMORES_3':back_right,
-             'SMORES_4':back_left, 'SMORES_5':front_right,'SMORES_6':front_left,}
+             'SMORES_4':back_left, 'SMORES_5':front_left,'SMORES_6':front_right,}
 NeutralPositions = {}
 
 disabledDof=['pan']
 speed_scale=0.7
-behaviorFiles = {
-   'ProboscisOverBar.xml':'ProboscisOverBar.xml',
-   'ProboscisPickup.xml':'ProboscisPickup.xml',
-   'ProboscisBackOverBar.xml':'ProboscisBackOverBar.xml',
-   'init':'ProboscisPostReconf.xml',
-   }
-
+behaviorFiles = {'lift':'ProboscisLift.xml',
+                 'drop':'ProboscisDrop.xml',
+                }
+#behaviorFiles = {
+#   'ProboscisOverBar.xml':'ProboscisOverBar.xml',
+#   'ProboscisPickup.xml':'ProboscisPickup.xml',
+#   'ProboscisBackOverBar.xml':'ProboscisBackOverBar.xml',
+#   'init':'ProboscisPostReconf.xml',
+#   }
+#
